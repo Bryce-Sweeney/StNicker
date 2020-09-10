ed3169bb74da5b3bd8497046d66aad9f290dc112
Bryce-Sweeney <70521786+Bryce-Sweeney@users.noreply.github.com>
2020-09-10 19:28:03 -0400

    Update

--- src/me/CodeConduit/StNicker/Main.java
+++ src/me/CodeConduit/StNicker/Main.java
@@ -1,7 +1,10 @@
 package me.CodeConduit.StNicker;
 
+import me.CodeConduit.StNicker.commands.NickCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
+    public void onEnable() {
+        new NickCommand(this);
+    }
 }
